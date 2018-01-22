06f43e3d5651fa0cdf8d76553573695dff5b3410
cadecahalan <cadecahalan@berkeley.edu>
2018-01-22 22:25:28 +0000

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,24 @@
+while (firstColorSeen != "red") {
+  down
+}
+right
+right
+while (firstColorSeen != "red") {
+  up
+}
+right
+right
+while (firstColorSeen != "red") {
+  down
+}
+right
+right
+while (firstColorSeen != "red") {
+  up
+}
+right
+right
+while (firstColorSeen != "red") {
+  down
+}
+right
