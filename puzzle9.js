9335fc081e9eed720ae5c106050842581e432060
cadecahalan <cadecahalan@berkeley.edu>
2018-01-23 02:00:57 +0000

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,29 @@
+var setColor == "blue"
+
+var steps = 0
+while (steps < 10) {
+  down
+  setColor
+  steps = steps + 1
+}
+
+var steps = 0
+while (steps < 10) {
+  right
+  setColor
+  steps = steps + 1
+}
+
+var steps = 0
+while (steps < 10) {
+  up
+  setColor
+  steps = steps + 1
+}
+
+var steps = 0
+while (steps < 10) {
+  left
+  setColor
+  steps = steps + 1
+}
