80338b9a16af36801b211554f3b8697b17d1212b
cadecahalan <cadecahalan@berkeley.edu>
2018-01-25 10:56:17 -0800

    Update puzzle8.js

--- puzzle8.js
+++ puzzle8.js
@@ -1,34 +1,34 @@
 down
 var steps = 0
-while (steps < 8) {
+while (steps < 7) {
   down
   steps = steps + 1
 }
 right
 right
 var steps = 0
-while (steps < 8) {
+while (steps < 7) {
   up
   steps = steps + 1
 }
 right
 right
 var steps = 0
-while (steps < 8) {
+while (steps < 7) {
   down
   steps = steps + 1
 }
 right
 right
 var steps = 0
-while (steps < 8) {
+while (steps < 7) {
   up
   steps = steps + 1
 }
 right
 right
 var steps = 0
-while (steps < 8) {
+while (steps < 7) {
   down
   steps = steps + 1
 }
